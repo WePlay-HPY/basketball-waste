fb8ca5740807619c6b68c63522d5cd5635674e98
PETER-PC-PORTAB\Peter <pierre-erik.lay@orange.fr>
2017-01-12 17:16:05 +0100

    Essaie changement fond d'ecran

---
+++ Assets/ImageBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageBehavior : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject basket = GameObject.FindGameObjectWithTag("Player");
+        basket.GetComponent<Renderer>().material.mainTexture = Resources.Load("pic.jpg") as Texture2D;
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
