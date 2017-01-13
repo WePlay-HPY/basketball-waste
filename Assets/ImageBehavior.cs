68063893e29a8072e6aa96cf55a495760bf12d0e
PETER-PC-PORTAB\Peter <pierre-erik.lay@orange.fr>
2017-01-13 09:05:20 +0100

    Changement d'image Windows

--- Assets/ImageBehavior.cs
+++ Assets/ImageBehavior.cs
@@ -1,17 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ImageBehavior : MonoBehaviour {
+    public WWW w;
 
     // Use this for initialization
     void Start()
     {
-        GameObject basket = GameObject.FindGameObjectWithTag("Player");
-        basket.GetComponent<Renderer>().material.mainTexture = Resources.Load("pic.jpg") as Texture2D;
+        // w = new WWW("file:///c:/Users/Peter/Desktop/pic.jpg");
+        w = new WWW("file:///sdcard/Download/pic.jpg");
+
     }
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (w.isDone)
+        {
+            Debug.Log("J'essaie de changer l'image");
+            GameObject basket = GameObject.FindGameObjectWithTag("Player") as GameObject;
+            Debug.Log(basket.name);
+            Texture2D texture = w.texture;
+            // Texture2D texture = Resources.Load<Texture2D>("pic");
+            Debug.Log(texture.name);
+            Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+
+            basket.GetComponent<SpriteRenderer>().sprite = sprite;
+
+        }
+    }
 }
